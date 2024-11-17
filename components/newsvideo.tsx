ccff8bdc07aa0e3716102ac5cbd66104bed997d4
William Fotso <will.fotso.pro@gmail.com>
2024-11-16 20:22:52 -0600

    Fix Video Dimensions

--- components/newsvideo.tsx
+++ components/newsvideo.tsx
@@ -1,17 +1,51 @@
 import { Video } from "@/utils/types";
+import { useEffect, useRef, useState } from "react";
 import YouTube from "react-youtube"
 
 function NewsVideo({ video }: { video: Video }) {
-    const opts = {
-        height: "390",
-        width: "640",
-        playerVars: {
-            autoplay: 0,
-        },
-    };
+
+    var containerRef = useRef(null)
+    const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+    const [opts, setOpts] = useState({})
+
+
+
+
+    useEffect(() => {
+
+        const observer = new ResizeObserver(entries => {
+            for (let entry of entries) {
+                const { width, height } = entry.contentRect;
+                setDimensions({ width, height });
+            }
+        });
+
+        if (containerRef.current) {
+            observer.observe(containerRef.current);
+        }
+
+        return () => observer.disconnect();
+
+    }, [])
+
+    useEffect(() => {
+        setOpts(
+            {
+                height: dimensions.height,
+                width: dimensions.width,
+                playerVars: {
+                    autoplay: 0,
+                },
+
+            }
+        )
+    }, [dimensions])
+
+
+
 
     return (
-        <div className="rounded-md border-2 border-gray-700 overflow-hidden">
+        <div id="parent" ref={containerRef} className="w-full h-full rounded-md border-2 border-gray-700 overflow-hidden">
             <YouTube videoId={video.video_id}
                 opts={opts} />
         </div>
